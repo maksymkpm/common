6c4969601764e3e21eb16563e8b24713b86aa094
Your Name <you@example.com>
2017-03-14 12:42:00 +0200

    joined methods for issue classification

--- classes/Issue.php
+++ classes/Issue.php
@@ -4,22 +4,31 @@ class Issue {
 	public function Get($issueId = 0) {
 		return true;
 	}
-	
+
 	public function Save($issueId = 0) {
 		return true;
 	}
-	
+
 	public function Update($issueId = 0) {
 		return true;
 	}
-	
+
 	public function Delete($issueId = 0) {
 		return true;
 	}
+
+	public static function ReturnIssueClassifications($language = 'ru'): array {
+		return [
+			'classes' => self::ReturnIssueClasses($language),
+			'objects' => self::ReturnIssueObjects($language),
+			'subjects' => self::ReturnIssueSubjects($language),
+			'categories' => self::ReturnIssueCategories($language),
+		];
+	}
 	
-	public static function ReturnIssueClasses($language = 'ru'): array {
+	private static function ReturnIssueClasses($language = 'ru'): array {
 		$classes = [
-			'ru' => 
+			'ru' =>
 				[
 					'1' => 'Взаимоотношения с семьей',
 					'2' => 'Коллектив (работа, учеба)',
@@ -30,7 +39,40 @@ class Issue {
 					'7' => 'Другое',
 				]
 		];
-		
+
 		return $classes[$language];
+	}
+	
+	private static function ReturnIssueObjects($language = 'ru'): array {
+		$objects = [
+			'ru' =>
+				[
+					'1' => 'Object1',
+				]
+		];
+
+		return $objects[$language];
+	}
+	
+	private static function ReturnIssueSubjects($language = 'ru'): array {
+		$subjects = [
+			'ru' =>
+				[
+					'1' => 'Subject1',
+				]
+		];
+
+		return $subjects[$language];
+	}
+	
+	private static function ReturnIssueCategories($language = 'ru'): array {
+		$categories = [
+			'ru' =>
+				[
+					'1' => 'Category1',
+				]
+		];
+
+		return $categories[$language];
 	}
 }
