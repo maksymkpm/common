f2678d6e8d9faeb063aad52be03f6e7ac2057ad8
Your Name <you@example.com>
2017-03-14 08:31:46 +0200

    added issue class

---
+++ classes/Issue.php
@@ -0,0 +1,20 @@
+<?php
+
+class Issue {
+	public function Get($issueId = 0) {
+		return true;
+	}
+	
+	public function Save($issueId = 0) {
+		return true;
+	}
+	
+	public function Update($issueId = 0) {
+		return true;
+	}
+	
+	public function Delete($issueId = 0) {
+		return true;
+	}
+	
+}
