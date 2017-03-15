7dbb0827ecc82ad0773cbb92133963409fe295b8
Your Name <you@example.com>
2017-03-15 15:46:38 +0200

    issue class updated

---
+++ classes/RequestParameters/IssueEdit.php
@@ -0,0 +1,17 @@
+<?php
+namespace RequestParameters;
+
+class IssueEdit extends IssueRequestParameters {
+	protected $issue_id;
+	protected $member_id;
+	protected $title;
+	protected $description;
+	protected $class_id;
+	protected $category_id;
+	protected $object_id;
+	protected $subject_id;
+	protected $priorit;
+	protected $status;
+	protected $date_added;
+	protected $comments_amount;
+}
