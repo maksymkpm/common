5d5c6e9e41e9c8d089e5a57c855f4e5a61e8db58
Maksym Dubovenko <Maksym Dubovenko>
2017-03-18 13:00:27 +0200

    feedback updated

--- classes/Feedback.php
+++ classes/Feedback.php
@@ -8,19 +8,31 @@ class Feedback {
             throw new \RuntimeException('Feedback data is empty.');
         }
 
-        $FeedbackIssue = [
+        $data = [
             'issue_id' => $property->issue_id,
             'member_id' => $property->member_id,
             'helpful' => $property->helpful,
             'date_added' => \db::expression('UTC_TIMESTAMP()'),
         ];
 
-        new \FormValidation($FeedbackIssue, 'FeedbackIssue');
+        new \FormValidation($data, 'FeedbackIssue');
 
-        return self::feedbackDatabase()
+        $result = self::feedbackDatabase()
             ->insert('feedback_issue')
-            ->values($FeedbackIssue)
+            ->values($data)
             ->execute();
+
+        if (!$result) {
+            throw new RuntimeException('Feedback didnt added.');
+        }
+
+        $result = self::updateIssueHelpfulCount($data['issue_id'], $data['helpful']);
+
+        if (!$result) {
+            throw new RuntimeException('Feedback didnt updated.');
+        }
+
+        return $result;
     }
 
     public static function CommentCreate(\RequestParameters\FeedbackCommentCreate $property) {
@@ -28,19 +40,61 @@ class Feedback {
             throw new \RuntimeException('Feedback data is empty.');
         }
 
-        $FeedbackIssueComment = [
+        $data = [
             'comment_id' => $property->comment_id,
             'member_id' => $property->member_id,
             'helpful' => $property->helpful,
             'date_added' => \db::expression('UTC_TIMESTAMP()'),
         ];
 
-        new \FormValidation($FeedbackIssueComment, 'FeedbackIssueComment');
+        new \FormValidation($data, 'FeedbackIssueComment');
 
-        return self::feedbackDatabase()
+        $result = self::feedbackDatabase()
             ->insert('feedback_issue_comment')
-            ->values($FeedbackIssueComment)
+            ->values($data)
             ->execute();
+
+        if (!$result) {
+            throw new RuntimeException('Feedback didnt added.');
+        }
+
+        $result = self::updateCommentHelpfulCount($data['comment_id'], $data['helpful']);
+
+        if (!$result) {
+            throw new RuntimeException('Feedback didnt updated.');
+        }
+
+        return $result;
+    }
+
+    private static function updateCommentHelpfulCount($comment_id, $helpful) {
+        $field = 'helpful';
+        if ($helpful == 0) {
+            $field = 'not_helpful';
+        }
+
+        $query = "
+			UPDATE issue_comment
+			SET {$field} = {$field} + 1
+			WHERE comment_id = :comment_id
+		";
+
+        return self::feedbackDatabase()->query($query, [':comment_id' => $comment_id])->execute();
+    }
+
+    private static function updateIssueHelpfulCount($issue_id, $helpful) {
+        $field = 'helpful';
+        if ($helpful == 0) {
+            $field = 'not_helpful';
+        }
+
+        $query = "
+			UPDATE issue
+			SET {$field} = {$field} + 1
+			WHERE issue_id = :issue_id
+		";
+
+      return self::feedbackDatabase()->query($query, [':issue_id' => $issue_id])->execute();
     }
 
     public static function feedbackDatabase(): \db {
